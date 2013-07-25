b4a6a5f8685c27bad3faeb51dfc1d838e9c666b4
timestep <jbosshellokitty@gmail.com>
2013-07-25 18:04:04 -0400

    working tests

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,19 +1,24 @@
 require 'spec_helper'
 
 describe "User" do 
+
 	it {should visit root_path}
 	
-	@user_attributes = FactoryGirl.attributes_for(:user)
-	@user = User.create(@user_attributes)
-	
-	context "attempts successful login" do
-		binding.pry
-		fill_in('Email', :with => @user_attributes[:email])
-		fill_in('Password', :with => @user_attributes[:password])
-		click_button('Log in')
-		it{should have_text("Logged in!")}
-	end
-	context "attempts failed login" do
+	before :each do
+		@user_attributes = FactoryGirl.attributes_for(:user)
+		@user = User.create(@user_attributes)
+		visit root_path
 	end
 
+	context 'in log phase' do
+		it "attempts successful login" do
+			fill_in('Email', :with => @user_attributes[:email])
+			fill_in('Password', :with => @user_attributes[:password])
+			click_button('Log in')
+			should have_text("Logged in!")
+		end
+
+		it "attempts failed login" do
+		end
+	end
 end
--- spec/features/visitor_spec.rb
+++ spec/features/visitor_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
 describe "Visitor" do 
-	context "should visit home page"
+	context "should visit home page" do
 		it {should visit root_path}
 	end
-	context "should be able to see log in page with form"
+	context "should be able to see log in page with form" do
 		it {should have_selector('form')}
 		it {should have_link('Log In')}
 	end
