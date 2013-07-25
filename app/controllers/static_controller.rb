0aa333c39c4f3a4b4e95d1443f7a850d8153c891
timestep <jbosshellokitty@gmail.com>
2013-07-25 16:44:47 -0400

    first test is green

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,4 @@
+class StaticController < ApplicationController
+	def login	
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ AdMan::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static#login'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
