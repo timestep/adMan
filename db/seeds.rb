3f9f303757f080415217cafd1d0d3b6ea352153d
timestep <jbosshellokitty@gmail.com>
2013-07-26 18:20:29 -0400

    prep for merge

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.destroy_all
+Booking.destroy_all
+Client.destroy_all
+Page.destroy_all
 
 User.create(
 	email: 'test',
