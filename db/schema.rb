82a8f91b949b1159c7a036746dae92a90bafbadc
timestep <jbosshellokitty@gmail.com>
2013-07-28 15:38:02 -0400

    test fixes

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20130726200826) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "bookings", force: true do |t|
     t.datetime "date"
     t.integer  "user_id"
