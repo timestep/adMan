b375b34c019d8ccbc7a5b3300ac8fc06771306c6
timestep <jbosshellokitty@gmail.com>
2013-07-28 16:06:47 -0400

    passes new client test

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,4 +1,17 @@
 class ClientsController < ApplicationController
+
+	before_filter :require_login
+	
 	def new
+		@client = Client.new
+	end
+
+	def create
+		@client = Client.new
+		if @client.save
+  		redirect_to bookings_path, :notice => "Created new client~!"
+  	else
+  		render :new
+  	end
 	end
 end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -45,12 +45,22 @@ describe "User" do
 		it "can navigate to 'add new pages' page" do
 			login(@user_attributes)
 			click_link("Add New Page")
+
 		end
 
 		it "can navigate to 'add new client' page" do
 			login(@user_attributes)
 			click_link("Add New Client")
-						
+		end
+	end
+	context 'while logged in and on add new client page' do
+		it "successfully adds a new client" do
+			login(@user_attributes)
+			click_link("Add New Client")
+			@client = FactoryGirl.create(:client)
+			fill_in('Name', :with => @client[:name])
+			binding.pry
+			click_button("Add Client")
 		end
 	end
 end
