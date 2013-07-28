225f8a719ad61d3bc1a1ffb5b5102997fed4e9d8
blibotics <blibotics@gmail.com>
2013-07-28 16:53:38 -0400

    Updating new action in Pages Controller and creating Create action in Pages Controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
   def new
+    @page = Page.new
+  end
+
+  def create
+    @page = Page.new
+    if @page.save
+      redirect_to bookings_path, :notice => "Created new page!"
+    else
+      render :new
+    end
   end
 end
