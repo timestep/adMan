6a27e64508dbe539f9c5b1452927746b1c752163
blibotics <blibotics@gmail.com>
2013-07-29 20:45:22 -0400

    Refactoring code

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -9,9 +9,9 @@ class Booking < ActiveRecord::Base
 
  	def self.search_date(value)
     value = DateTime.strptime(value, "%m/%d/%Y")
+
     beginning_of_day = value
     end_of_day = beginning_of_day + 1.day
-
     result = Booking.where(:date => beginning_of_day..end_of_day)
  		# result = Booking.find(:all, 
  		# 	:conditions => {:date => value})
