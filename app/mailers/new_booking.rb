d3ce3cfde187190d2880a95ec3c041248f4868d6
timestep <jbosshellokitty@gmail.com>
2013-08-01 13:17:24 -0400

    added new booking email method

---
+++ app/mailers/new_booking.rb
@@ -0,0 +1,9 @@
+class NewBooking < ActionMailer::Base
+  default from: "from@example.com"
+
+  def new_booking(user,booking)
+  	@user = user
+  	@booking = booking
+  	mail(to: @user.email, subject: 'New Booking')
+  end
+end
