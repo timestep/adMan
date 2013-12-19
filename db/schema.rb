7e7b562196e2a902569429f86847af0f8b016f12
Kevin Sivabalan <kevin@buzzbuzzhome.com>
2013-12-19 11:16:34 -0500

    whaty

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130801072934) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookings", force: true do |t|
     t.datetime "date"
     t.integer  "user_id"
